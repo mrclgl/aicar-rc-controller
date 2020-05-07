1c3acdfdcca0580ac0ff774684cbdd5e16fe2e56
Check2016 <info@marcel-egle.de>
2020-05-07 22:43:22 +0200

    added datasheet

--- RCController/RCController.ino
+++ RCController/RCController.ino
@@ -1,8 +1,21 @@
 
+#include <Wire.h>
+
 void setup() {
   Serial.begin(115200);
+  Wire.begin(4);
+  Wire.onReceive(receiveEvent);
 }
 
 void loop() {
   
+}
+
+void receiveEvent(int howMany)
+{
+  while(Wire.available()) // loop through all but the last
+  {
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
 }
