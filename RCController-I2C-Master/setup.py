937bda34121cbfa0a5a94343a57937ac7f38bb7f
Check2016 <info@marcel-egle.de>
2020-05-20 18:56:40 +0200

    added setup.py for python lib

---
+++ RCController-I2C-Master/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="rccontroller-i2c-lib",
+    version="1.0.0",
+    author="Marcel Egle",
+    author_email="info@marcel-egle.de",
+    description="RCController I2C-Master Library",
+    url="https://github.com/check2016",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
